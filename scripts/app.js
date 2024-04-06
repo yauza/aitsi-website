c1a27fbf1ea2f09476fc69fb7382178f0ef64e67
yauza <jan.stobnicki@risco.net.pl>
2024-04-06 16:27:08 +0200

    Add bootstrap sections, fix navbar

---
+++ scripts/app.js
@@ -0,0 +1,24 @@
+(async function() {
+    const chart = document.getElementById('chart1');
+    new Chart(
+        chart, 
+        {
+            type: 'bar',
+            data: {
+                labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+                datasets: [{
+                    label: '# of Votes',
+                    data: [12, 19, 3, 5, 2, 3],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+      });
+
+})();
